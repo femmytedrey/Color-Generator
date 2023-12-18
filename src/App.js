5dc5f6dfe78125d38f64b98037d09e6907e61050
femmytedrey <femmytedrey@gmail.com>
2023-12-18 14:42:43 +0100

    copy to clipboard implemented

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#f15025').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,6 +21,7 @@ function App() {
   }
   return <>
     <section className='container'>
+      <h3>color generator</h3>
       <form onSubmit={handleSubmit}>
         <input type='text' value={color} onChange={(e) => setColor(e.target.value)} placeholder='#f15025' className={`${error ? 'error' : null}`} />
         <button className='btn' type='submit'>submit</button>
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -6,9 +6,25 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const bcg = rgb.join(',');
   const hex = rgbToHex(...rgb)
   const hexValue = `#${hexColor}`
-  return (<article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}}>
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+    return () => clearTimeout(timeout);
+  }, [alert])
+
+  return (<article 
+            className={`color ${index > 10 && 'color-light'}`}
+            style={{backgroundColor:`rgb(${bcg})`}}
+            onClick={() => {
+              setAlert(true)
+              navigator.clipboard.writeText(hexValue)
+            }}
+          >
     <p className='percent-value'>%{weight}</p>
     <p className='color-value'>{hexValue}</p>
+    {alert && <p className='alert'>copied to clipboard</p>}
   </article>
   )
 }
